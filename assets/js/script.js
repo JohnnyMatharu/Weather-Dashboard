bd90d66f5b3fef27e87463b7fdaf17f1026c9174
Johnny <jaani.matharu@gmail.com>
2021-03-30 12:14:54 -0400

    made revisions to fetch, still few errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,17 +4,20 @@ document.getElementById("textEntryBtn").addEventListener("click", enterRequest);
 function enterRequest(){
 entry = document.getElementById("textEntry").value;
 console.log(entry);
-var getUserDate = function(userInput) {
-  var url = "api.openweathermap.org/data/2.5/weather?q=" + entry + "&appid={1686d3ccdcbc85f85b97533207c961cf}";
-fetch(url).then(function(response) {
-    response.json().then(function(data) {
+
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" + entry + "&appid=1686d3ccdcbc85f85b97533207c961cf";
+console.log(url);
+ fetch(url).then(response => response.json()) 
+   .then(data => console.log(data)); 
      console.log(data);
-    //   document.getElementById("dailyWeather").innerHTML = data;
+ 
+ 
+ 
+ 
+ 
+     //   document.getElementById("dailyWeather").innerHTML = data;
 //  document.getElementById("dailyForecast").innerHTML = data;
-     });
-   });
- };
-}
+   };
 
 
 //anotherFunction() to sort two types of data
