99153507d3bacfc9daaeee9535f2c9a41e164e1e
Johnny <jaani.matharu@gmail.com>
2021-03-30 12:49:51 -0400

    one fetch for weather is working, now other one for forecast needs to be fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 var entry = 0;
 document.getElementById("textEntryBtn").addEventListener("click", enterRequest);
 
-function enterRequest(){
+function enterRequest(event){
+    event.preventDefault();
 entry = document.getElementById("textEntry").value;
 console.log(entry);
 
@@ -9,23 +10,16 @@ var url = "https://api.openweathermap.org/data/2.5/weather?q=" + entry + "&appid
 console.log(url);
  fetch(url).then(response => response.json()) 
    .then(data => console.log(data)); 
-     console.log(data);
- 
- 
- 
- 
- 
+
+
+
+
+   
      //   document.getElementById("dailyWeather").innerHTML = data;
 //  document.getElementById("dailyForecast").innerHTML = data;
    };
-
-
 //anotherFunction() to sort two types of data
 
 //displayData2() current data
 
 // displayData2() 5 days forecast
-
-
-
-
