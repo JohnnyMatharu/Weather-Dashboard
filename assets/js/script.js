6e11a3cc69f91c3cf8cb8ac0e417eb1eab4592bb
Johnny <jaani.matharu@gmail.com>
2021-03-31 16:09:57 -0400

    remaining UV index changing colour, weather condition icon, and local storage appendchild buttons to research

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,9 @@ document.getElementById("textEntryBtn").addEventListener("click", enterRequest);
 
 function enterRequest(event){
     event.preventDefault();
-entry = document.getElementById("textEntry").value;
+entry = document.getElementById("textCityEntry").value;
 console.log(entry);
+//entryUV = document.getElementById("textProvinceEntry").value;
 
 var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + entry + "&appid=1686d3ccdcbc85f85b97533207c961cf";
 console.log(urlWeather);
@@ -21,6 +22,20 @@ console.log(urlWeather);
      
    }); 
    
+var urlUV = "https://api.weatherbit.io/v2.0/current?&city=" + entry + "&key=109cad5ca78c416a9cb53bd3881be245";
+//var urlUV = "https://api.weatherbit.io/v2.0/current?&city=" + entry + "," + entryUV + "&key=109cad5ca78c416a9cb53bd3881be245";
+   console.log(urlUV);
+    fetch(urlUV).then(response => response.json()) 
+      .then(data => {
+        console.log(data.data[0].uv);
+        document.getElementById("uv").innerHTML = "UV Index: " + data.data[0].uv;
+        //enter date
+      }); 
+
+
+
+
+
  var urlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + entry + "&appid=1686d3ccdcbc85f85b97533207c961cf";
 console.log(urlForecast);
   fetch(urlForecast).then(response => response.json()) 
@@ -31,23 +46,23 @@ console.log(urlForecast);
      var date = (DateTime.now().toFormat("yyyy/MM/dd"));
      document.getElementById("currentDate").innerHTML = date;
      // document.getElementById("currentDate").innerHTML = "Date: " + data.list[0].dt_txt; 
-    document.getElementById("dateForecastOne").innerHTML = "Date: " + data.list[6].dt_txt;
+    document.getElementById("dateForecastOne").innerHTML = "Date & time: " + data.list[6].dt_txt;
     document.getElementById("temperatureForecastOne").innerHTML = "Temperature: " + data.list[6].main.temp;
     document.getElementById("humidityForecastOne").innerHTML = "Humidity: " + data.list[6].main.humidity;
 
-    document.getElementById("dateForecastTwo").innerHTML = "Date: " + data.list[14].dt_txt;
+    document.getElementById("dateForecastTwo").innerHTML = "Date & time: " + data.list[14].dt_txt;
     document.getElementById("temperatureForecastTwo").innerHTML = "Temperature: " + data.list[14].main.temp;
     document.getElementById("humidityForecastTwo").innerHTML = "Humidity: " + data.list[14].main.humidity;
     
-    document.getElementById("dateForecastThree").innerHTML = "Date: " + data.list[22].dt_txt;
+    document.getElementById("dateForecastThree").innerHTML = "Date & time: " + data.list[22].dt_txt;
     document.getElementById("temperatureForecastThree").innerHTML = "Temperature: " + data.list[22].main.temp;
     document.getElementById("humidityForecastThree").innerHTML = "Humidity: " + data.list[22].main.humidity;
 
-    document.getElementById("dateForecastFour").innerHTML = "Date: " + data.list[30].dt_txt;
+    document.getElementById("dateForecastFour").innerHTML = "Date & time: " + data.list[30].dt_txt;
     document.getElementById("temperatureForecastFour").innerHTML = "Temperature: " + data.list[30].main.temp;
     document.getElementById("humidityForecastFour").innerHTML = "Humidity: " + data.list[30].main.humidity;
     
-    document.getElementById("dateForecastFive").innerHTML = "Date: " + data.list[38].dt_txt;
+    document.getElementById("dateForecastFive").innerHTML = "Date & time: " + data.list[38].dt_txt;
     document.getElementById("temperatureForecastFive").innerHTML = "Temperature: " + data.list[38].main.temp;
     document.getElementById("humidityForecastFive").innerHTML = "Humidity: " + data.list[38].main.humidity;
    
