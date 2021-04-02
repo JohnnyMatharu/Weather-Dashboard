3461dbb6d4bb4c4124b777f96a920966cd97235d
Johnny <jaani.matharu@gmail.com>
2021-04-02 14:51:17 -0400

    history buttons are now working

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,12 +16,73 @@ newLine.textContent = displayArr[i];
 
 newLine.id = displayArr[i];
 //new code
-newLine.addEventListener("click", (ev) => console.log(ev.target.textContent))
+newLine.addEventListener("click", (ev) => historyWeather(ev.target.textContent));
 //new code
 list.appendChild(newLine);
 
 }
+function historyWeather(displayLine){
+  let histEntry = displayLine;
+ 
+   //entryUV = document.getElementById("textProvinceEntry").value;
+  
+  var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + histEntry + "&appid=1686d3ccdcbc85f85b97533207c961cf";
+   console.log(urlWeather);
+    fetch(urlWeather).then(response => response.json()) 
+      .then(data => {
+        console.log(data);
+        document.getElementById("currentCity").innerHTML = data.name;
+        //enter date
+        //console.log(data.main.temp);
+       document.getElementById("temperature").innerHTML = "Temperature: " + data.main.temp;
+       document.getElementById("humidity").innerHTML = "Humidity: " + data.main.humidity;
+       document.getElementById("windSpeed").innerHTML = "Wind Speed: " + data.wind.speed;
+       //enter uv index from another site if possible
+       
+     }); 
+     
+   var urlUV = "https://api.weatherbit.io/v2.0/current?&city=" + histEntry + "&key=109cad5ca78c416a9cb53bd3881be245";
+   //var urlUV = "https://api.weatherbit.io/v2.0/current?&city=" + entry + "," + entryUV + "&key=109cad5ca78c416a9cb53bd3881be245";
+      console.log(urlUV);
+       fetch(urlUV).then(response => response.json()) 
+         .then(data => {
+           console.log(data.data[0].uv);
+           document.getElementById("uv").innerHTML = "UV Index: " + data.data[0].uv;
+           //enter date
+         }); 
 
+    var urlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + histEntry + "&appid=1686d3ccdcbc85f85b97533207c961cf";
+   console.log(urlForecast);
+     fetch(urlForecast).then(response => response.json()) 
+       .then(data => { 
+         // change below
+      //  console.log(data.list[0].dt_txt);
+        var DateTime = luxon.DateTime;
+        var date = (DateTime.now().toFormat("yyyy/MM/dd"));
+        document.getElementById("currentDate").innerHTML = date;
+        // document.getElementById("currentDate").innerHTML = "Date: " + data.list[0].dt_txt; 
+       document.getElementById("dateForecastOne").innerHTML = "Date & time: " + data.list[6].dt_txt;
+       document.getElementById("temperatureForecastOne").innerHTML = "Temperature: " + data.list[6].main.temp;
+       document.getElementById("humidityForecastOne").innerHTML = "Humidity: " + data.list[6].main.humidity;
+  
+       document.getElementById("dateForecastTwo").innerHTML = "Date & time: " + data.list[14].dt_txt;
+       document.getElementById("temperatureForecastTwo").innerHTML = "Temperature: " + data.list[14].main.temp;
+       document.getElementById("humidityForecastTwo").innerHTML = "Humidity: " + data.list[14].main.humidity;
+      
+       document.getElementById("dateForecastThree").innerHTML = "Date & time: " + data.list[22].dt_txt;
+       document.getElementById("temperatureForecastThree").innerHTML = "Temperature: " + data.list[22].main.temp;
+       document.getElementById("humidityForecastThree").innerHTML = "Humidity: " + data.list[22].main.humidity;
+  
+       document.getElementById("dateForecastFour").innerHTML = "Date & time: " + data.list[30].dt_txt;
+       document.getElementById("temperatureForecastFour").innerHTML = "Temperature: " + data.list[30].main.temp;
+       document.getElementById("humidityForecastFour").innerHTML = "Humidity: " + data.list[30].main.humidity;
+      
+       document.getElementById("dateForecastFive").innerHTML = "Date & time: " + data.list[38].dt_txt;
+       document.getElementById("temperatureForecastFive").innerHTML = "Temperature: " + data.list[38].main.temp;
+       document.getElementById("humidityForecastFive").innerHTML = "Humidity: " + data.list[38].main.humidity;
+     
+     });
+};
 
 //var displayLineFixed = JSON.parse(localStorage.getItem("cityName")) || [];
 //document.getElementById("searchHistory").innerHTML=displayLineFixed;
@@ -45,78 +106,13 @@ const list = document.getElementById("searchHistory");
 
 let newLine = document.createElement('button');
 
-// function historyWeather(ev, displayLine){
-//     let histEntry = displayLine;
-//     console.log(ev.target, histEntry);
-//     //entryUV = document.getElementById("textProvinceEntry").value;
-    
-//     var urlWeather = "https://api.openweathermap.org/data/2.5/weather?q=" + histEntry + "&appid=1686d3ccdcbc85f85b97533207c961cf";
-//     console.log(urlWeather);
-//      fetch(urlWeather).then(response => response.json()) 
-//        .then(data => {
-//          console.log(data);
-//          document.getElementById("currentCity").innerHTML = data.name;
-//          //enter date
-//          //console.log(data.main.temp);
-//         document.getElementById("temperature").innerHTML = "Temperature: " + data.main.temp;
-//         document.getElementById("humidity").innerHTML = "Humidity: " + data.main.humidity;
-//         document.getElementById("windSpeed").innerHTML = "Wind Speed: " + data.wind.speed;
-//         //enter uv index from another site if possible
-         
-//        }); 
-       
-//     var urlUV = "https://api.weatherbit.io/v2.0/current?&city=" + histEntry + "&key=109cad5ca78c416a9cb53bd3881be245";
-//     //var urlUV = "https://api.weatherbit.io/v2.0/current?&city=" + entry + "," + entryUV + "&key=109cad5ca78c416a9cb53bd3881be245";
-//        console.log(urlUV);
-//         fetch(urlUV).then(response => response.json()) 
-//           .then(data => {
-//             console.log(data.data[0].uv);
-//             document.getElementById("uv").innerHTML = "UV Index: " + data.data[0].uv;
-//             //enter date
-//           }); 
-
-//      var urlForecast = "https://api.openweathermap.org/data/2.5/forecast?q=" + histEntry + "&appid=1686d3ccdcbc85f85b97533207c961cf";
-//     console.log(urlForecast);
-//       fetch(urlForecast).then(response => response.json()) 
-//         .then(data => { 
-//           // change below
-//        //  console.log(data.list[0].dt_txt);
-//          var DateTime = luxon.DateTime;
-//          var date = (DateTime.now().toFormat("yyyy/MM/dd"));
-//          document.getElementById("currentDate").innerHTML = date;
-//          // document.getElementById("currentDate").innerHTML = "Date: " + data.list[0].dt_txt; 
-//         document.getElementById("dateForecastOne").innerHTML = "Date & time: " + data.list[6].dt_txt;
-//         document.getElementById("temperatureForecastOne").innerHTML = "Temperature: " + data.list[6].main.temp;
-//         document.getElementById("humidityForecastOne").innerHTML = "Humidity: " + data.list[6].main.humidity;
-    
-//         document.getElementById("dateForecastTwo").innerHTML = "Date & time: " + data.list[14].dt_txt;
-//         document.getElementById("temperatureForecastTwo").innerHTML = "Temperature: " + data.list[14].main.temp;
-//         document.getElementById("humidityForecastTwo").innerHTML = "Humidity: " + data.list[14].main.humidity;
-        
-//         document.getElementById("dateForecastThree").innerHTML = "Date & time: " + data.list[22].dt_txt;
-//         document.getElementById("temperatureForecastThree").innerHTML = "Temperature: " + data.list[22].main.temp;
-//         document.getElementById("humidityForecastThree").innerHTML = "Humidity: " + data.list[22].main.humidity;
-    
-         document.getElementById("dateForecastFour").innerHTML = "Date & time: " + data.list[30].dt_txt;
-         document.getElementById("temperatureForecastFour").innerHTML = "Temperature: " + data.list[30].main.temp;
-         document.getElementById("humidityForecastFour").innerHTML = "Humidity: " + data.list[30].main.humidity;
-        
-         document.getElementById("dateForecastFive").innerHTML = "Date & time: " + data.list[38].dt_txt;
-         document.getElementById("temperatureForecastFive").innerHTML = "Temperature: " + data.list[38].main.temp;
-         document.getElementById("humidityForecastFive").innerHTML = "Humidity: " + data.list[38].main.humidity;
-       
-       });
- };
+ 
 
 newLine.textContent = displayLine;
 console.log(newLine.textContent);
 list.appendChild(newLine);
 console.log(newLine.textContent);
-newLine.addEventListener("click", historyWeather(ev, displayLine));
-
-
-
-
+newLine.addEventListener("click", (ev) => historyWeather(ev.target.textContent));
 
 
 
